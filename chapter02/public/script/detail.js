197a185d4b46d3720b2f3722381bc9f536629cab
liuruoran88 <liuruoran88@163.com>
2018-11-13 14:17:33 +0800

    demo细节修改

--- chapter02/public/script/detail.js
+++ chapter02/public/script/detail.js
@@ -1,5 +1,5 @@
 /**
- *  index.js 主页面js，事件绑定和处理
+ *  detail.js 详情页js，事件绑定和处理
  */
 
 'use strict'
@@ -7,9 +7,9 @@
 let app = {
 	sidebarShow: false,
 	loadingShow: true
-};
+}
 
-const url = './assets/mockData/index.json';
+const url = './assets/mockData/detail.json'
 
 /*******************
 * 页面方法
@@ -23,21 +23,21 @@ const url = './assets/mockData/index.json';
 app.getData = function(url) {
 
   // 请求最新数据
-  let xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest()
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      let response = JSON.parse(xhr.response);
-      let results = response.data.data;
-      // app.updateTemplate(results);
+      let response = JSON.parse(xhr.response)
+      let results = response.data.data
+      // app.updateTemplate(results)
 
       setTimeout(function delay() {
-      	app.updateTemplate(results);
+        app.updateTemplate(results)
       }, 3000)
     }
-  };
-  xhr.open('GET', url);
-  xhr.send();
-};
+  }
+  xhr.open('GET', url)
+  xhr.send()
+}
 
 /**
  * [updateTemplate 更新页面
@@ -45,13 +45,13 @@ app.getData = function(url) {
  * @return {[type]}              [description]
  */
 app.updateTemplate = function (data) {
-  let container = document.querySelector('.main');
+  let container = document.querySelector('.main')
   // 这里可以一些复杂的DOM结构来，这里仅给出简单的示例
-  container.textContent = data;
+  container.textContent = data
 }
 
 app.refresh = function () {
-	app.getData(url);
+  app.getData(url)
 }
 
 
@@ -61,38 +61,37 @@ app.refresh = function () {
 
 // sidebar btn
 document.querySelector('.sidebar').addEventListener('click', function () {
-	app.closeSidebar();
+	app.closeSidebar()
 })
 
 document.querySelector('.icon-category').addEventListener('click', function () {
-	app.openSidebar();
+	app.openSidebar()
 })
 
 document.querySelector('.mask').addEventListener('click', function () {
-	app.closeSidebar();
+	app.closeSidebar()
 })
 
 
 document.querySelector('.icon-refresh').addEventListener('click', function () {
-	app.refresh();
+	app.refresh()
 })
 
 
 app.openSidebar = function () {
-	if (app.sidebarShow) return;
-	document.querySelector('.mask').classList.remove('hide');
-	document.querySelector('.sidebar').classList.remove('hide');
-	app.sidebarShow = true;
+	if (app.sidebarShow) return
+	document.querySelector('.mask').classList.remove('hide')
+	document.querySelector('.sidebar').classList.remove('hide')
+	app.sidebarShow = true
 }
 
 app.closeSidebar = function () {
-	if (!app.sidebarShow) return;
-	document.querySelector('.mask').classList.add('hide');
-	document.querySelector('.sidebar').classList.add('hide');
-	app.sidebarShow = false;
+	if (!app.sidebarShow) return
+	document.querySelector('.mask').classList.add('hide')
+	document.querySelector('.sidebar').classList.add('hide')
+	app.sidebarShow = false
 }
 
-
 // 请求数据并更新
-app.getData(url);
+app.getData(url)
 	
