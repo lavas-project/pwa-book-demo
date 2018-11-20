85333645939b5b62a931e8ceae8163fcbf6fd686
liuruoran88 <liuruoran88@163.com>
2018-11-20 12:39:14 +0800

    细节调整

--- chapter02/server.js
+++ chapter02/server.js
@@ -3,39 +3,38 @@
  * express 搭建一个简单的 node 服务，便于调试
  */
 
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-
-let app = express();
-let port = 8088;
+let express = require('express')
+let path = require('path')
+let fs = require('fs')
+let app = express()
+let port = 8088
 
 
 // 设定静态文件目录，比如本地文件
 // 目录为pwa-book-demo/public/images，访问
 // 网址则显示为http://localhost:8088/images
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')))
 
 // 首页 html 响应
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: 'public'});
-});
+  res.sendFile('index.html', {root: 'public'})
+})
 app.get('/index.html', function (req, res) {
-    res.sendFile('index.html', {root: 'public'});
-});
+  res.sendFile('index.html', {root: 'public'})
+})
 
 app.get('/detail.html*', function (req, res) {
-    res.sendFile('detail.html', {root: 'public'});
-});
+  res.sendFile('detail.html', {root: 'public'})
+})
 
-app.get('/mockData/:id', function(req, res) {
-	res.json(getMockData('/mockData/' + req.params.id));
-});
+app.get('/mockData/:id', function (req, res) {
+	res.json(getMockData('/mockData/' + req.params.id))
+})
 
 // 启动服务器
 app.listen(port, function () {
-    console.log(`Server start on: http://127.0.0.1:${port}`);
-});
+  console.log(`Server start on: http://127.0.0.1:${port}`)
+})
 
 /**
  * 读取json数据
@@ -45,11 +44,11 @@ app.listen(port, function () {
  */
 function getMockData(dataPath) {
     try {
-        return JSON.parse(
-        	fs.readFileSync(path.join(__dirname, dataPath), {encoding: 'utf-8'}));
+      return JSON.parse(
+        fs.readFileSync(path.join(__dirname, dataPath), {encoding: 'utf-8'}))
     }
     catch (e) {
-        console.error(e.stack);
-        return {};
+      console.error(e.stack)
+      return {}
     }
 }
